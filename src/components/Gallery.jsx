e16a267bd7de73c652233364b50776b173e02383
jungeunvDev <jungeun.lee@teamsparta.co>
2023-09-04 16:51:01 +0900

    style: 화살표 디자인 수정

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -12,42 +12,90 @@ const Container = styled.div`
 const Image = styled.img`
   width: 100%;
   display: block;
+  border-radius: 5px;
 `;
 
 const Arrow = styled.button`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-  background: rgba(255, 255, 255, 0.7);
+  background-color: transparent;
+  color: white;
   border: none;
   cursor: pointer;
   font-size: 24px;
   padding: 10px;
-  transition: background 0.3s;
-  &:hover {
-    background: rgba(255, 255, 255, 0.9);
-  }
+  transition: 0.3s;
 `;
 
 const LeftArrow = styled(Arrow)`
   left: 10px;
 `;
 
+const BeforeArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M15 20L7 12L15 4"
+        stroke="#fff"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
 const RightArrow = styled(Arrow)`
   right: 10px;
 `;
 
+const NextArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g transform="scale(-1, 1) translate(-24, 0)">
+        <path
+          d="M15 20L7 12L15 4"
+          stroke="#fff"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
 export default function Gallery() {
   const images = [
-    "/images/header.jpeg",
-    "/images/test.jpeg",
-    "/images/header.jpeg",
-    "/images/header.jpeg",
-    "/images/header.jpeg",
-    "/images/header.jpeg",
-    "/images/header.jpeg",
-    "/images/header.jpeg",
-    "/images/header.jpeg",
+    "/images/1.jpeg",
+    "/images/2.jpeg",
+    "/images/3.jpeg",
+    "/images/4.jpeg",
+    "/images/5.jpeg",
+    "/images/6.jpeg",
+    "/images/7.jpeg",
+    "/images/8.jpeg",
+    "/images/9.jpeg",
+    "/images/10.jpeg",
+    "/images/11.jpeg",
+    "/images/12.jpeg",
+    "/images/13.jpeg",
+    "/images/14.jpeg",
+    "/images/15.jpeg",
+    "/images/16.jpeg",
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
@@ -68,10 +116,10 @@ export default function Gallery() {
       />
 
       <LeftArrow onClick={() => setCurrentImage((prev) => prev - 1)}>
-        &lt;
+        <BeforeArrow />
       </LeftArrow>
       <RightArrow onClick={() => setCurrentImage((prev) => prev + 1)}>
-        &gt;
+        <NextArrow />
       </RightArrow>
     </Container>
   );
