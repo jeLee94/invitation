417f46e83dbbd1493f502a3dfe6e220f55d8617d
jungeunvDev <jungeun.lee@teamsparta.co>
2023-09-05 01:22:26 +0900

    feat: 미리보기 이미지 변경

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -21,7 +21,8 @@ export const Share = () => {
         content: {
           title: "김성훈 🩷 장보경 결혼합니다.",
           description: "10월 9일 월요일 오후 2시",
-          imageUrl: "https://i.ibb.co/d012cp1/2023-09-04-10-36-11.png",
+          imageUrl:
+            "https://ibb.co/https://i.ibb.co/dDWbbCq/2023-09-04-10-36-11.png",
           link: {
             mobileWebUrl: "https://invitation-rust.vercel.app/",
             webUrl: "https://invitation-rust.vercel.app/",
